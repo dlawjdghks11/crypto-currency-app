2bf844236ac6631a2ec4ad1ece05285af18d8032
dlawjdghks11 <85840643+dlawjdghks11@users.noreply.github.com>
2025-02-27 05:29:51 +0900

    infoData, priceData api 추가

--- src/api.ts
+++ src/api.ts
@@ -5,3 +5,15 @@ export const getCoins = async () => {
 
   return res.data.slice(0, 100)
 }
+
+export const getInfoData = async (coinId: string) => {
+  const res = await axios.get(`https://api.coinpaprika.com/v1/coins/${coinId}`)
+
+  return res.data;
+}
+
+export const getPriceData = async (coinId: string) => {
+  const res = await axios.get(`https://api.coinpaprika.com/v1/tickers/${coinId}`)
+
+  return res.data;
+}
--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -1,7 +1,8 @@
 import { useQuery } from "@tanstack/react-query";
-import { useParams } from "react-router-dom";
+import { Outlet, useNavigate, useParams } from "react-router-dom";
 import styled from "styled-components";
-import { getCoinData } from "../api";
+import { getInfoData, getPriceData } from "../api";
+import { useState } from "react";
 
 const Container = styled.div`
   padding: 0px 20px;
@@ -45,39 +46,122 @@ const Description = styled.p`
   padding: 20px 0px;
 `;
 
+const Loader = styled.div`
+  color: ${(props) => props.theme.text};
+`;
+
+const Tab = styled.div`
+  display: flex;
+  background-color: black;
+  justify-content: space-between;
+  padding: 5px 100px;
+  border-radius: 10px;
+  margin-top: 20px;
+  border: 1px solid;
+`;
+
+const TabItem = styled.div<{
+  isActive: boolean;
+}>`
+  color: ${(props) => props.isActive ? props.theme.primary : props.theme.text};
+`;
+
+interface InfoData {
+  id: string;
+  name: string;
+  symbol: string;
+  rank: number;
+  is_new: boolean;
+  is_active: boolean;
+  type: string;
+  logo: string;
+  description: string;
+  message: string;
+  open_source: boolean;
+  started_at: string;
+  development_status: string;
+  hardware_wallet: boolean;
+  proof_type: string;
+  org_structure: string;
+  hash_algorithm: string;
+  first_data_at: string;
+  last_data_at: string;
+}
+
+interface PriceData {
+  id: string;
+  name: string;
+  symbol: string;
+  rank: number;
+  total_supply: number;
+  max_supply: number;
+  beta_value: number;
+  first_data_at: string;
+  last_updated: string;
+  quotes: object;
+}
+
 const Coin = () => {
-  const params = useParams()
+  const params = useParams();
+  const navigate = useNavigate();
   const coinId = params.coinId ?? "";
+  const [tabName, setTabName] = useState<string>()
+  const { isLoading: infoLoading, data: infoData } = useQuery<InfoData>({ queryKey: ["info", coinId], queryFn: () => getInfoData(coinId)})
+  const { isLoading: tickerLoading, data: tickersData } = useQuery<PriceData>({ queryKey: ["tickers", coinId], queryFn: () => getPriceData(coinId)})
+  const loading = infoLoading || tickerLoading;
+
+  const onClickTabMenu = (tabName: string) => {
+    setTabName(tabName);
+    navigate(tabName);
+  }
 
   return <Container>
     <Header>
-      <Title>{coinId}</Title>
+      <Title>{infoData?.name}</Title>
     </Header>
-    <Overview>
-      <OverviewItem>
-        <Text>Rank</Text>
-        <Text>1</Text>
-      </OverviewItem>
-      <OverviewItem>
-        <Text>SYMBOL</Text>
-        <Text>$BTC</Text>
-      </OverviewItem>
-      <OverviewItem>
-        <Text>OPEN SOURCE</Text>
-        <Text>YES</Text>
-      </OverviewItem>
-    </Overview>
-    <Description>dgsgsags</Description>
-    <Overview>
-      <OverviewItem>
-        <Text>TOTAL SUPPLY</Text>
-        <Text>13113</Text>
-      </OverviewItem>
-      <OverviewItem>
-        <Text>MAX SUPPLY</Text>
-        <Text>2100000</Text>
-      </OverviewItem>
-    </Overview>
+    {loading ? (
+      <Loader>Loading...</Loader>
+    ) : (
+      <>
+        <Overview>
+          <OverviewItem>
+            <Text>Rank</Text>
+            <Text>{infoData?.rank}</Text>
+          </OverviewItem>
+          <OverviewItem>
+            <Text>SYMBOL</Text>
+            <Text>$ {infoData?.symbol}</Text>
+          </OverviewItem>
+          <OverviewItem>
+            <Text>OPEN SOURCE</Text>
+            <Text>{infoData?.open_source ? "YES" : "NO"}</Text>
+          </OverviewItem>
+        </Overview>
+        <Description>{infoData?.description}</Description>
+        <Overview>
+          <OverviewItem>
+            <Text>TOTAL SUPPLY</Text>
+            <Text>{tickersData?.total_supply}</Text>
+          </OverviewItem>
+          <OverviewItem>
+            <Text>MAX SUPPLY</Text>
+            <Text>{tickersData?.max_supply}</Text>
+          </OverviewItem>
+        </Overview>
+        <Tab>
+          <TabItem 
+            onClick={() => onClickTabMenu("chart")}
+            isActive={tabName === "chart"}
+          >Chart</TabItem>
+          <TabItem 
+            onClick={() => onClickTabMenu("price")}
+            isActive={tabName === "price"}
+          >Price</TabItem>
+        </Tab>
+        <Outlet />
+      </>
+    )}
+    
   </Container>
 }
 
